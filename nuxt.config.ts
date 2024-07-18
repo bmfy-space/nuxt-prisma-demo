b6b4120610cad9c410ded793aeadaff37bea86e4
bmfy <bb0103@foxmail.com>
2024-07-18 17:02:08 +0800

    update: nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  pages: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ["@prisma/nuxt"],
